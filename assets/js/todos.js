1077d769e2f7b02fe17e1d230e731f462ebeb371
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-05-16 23:50:53 -0600

    Creating Todos implemented

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,24 +1,30 @@
 //Check off specific Todos by clicking
-$("li").click(function(){
-  // if($(this).css("color") === "rgb(128, 128, 128)"){
-  //   $(this).css({
-  //     color: "black",
-  //     textDecoration: "none"
-  //   });
-  // } else {
-  //   $(this).css({
-  //     color: "gray",
-  //     textDecoration: "line-through"
-  //   });
-  // }
+$("ul").on("click", "li", function(){
+//code runs when an li is clikced inside the ul
   $(this).toggleClass("completed");
 });
 
 
 //Delete Todo by clicking on X:
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
+  //code runs when an span is clikced inside the ul
   $(this).parent().fadeOut(500,function(){
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+//Codes for adding Todos
+//Notes
+// - Xter code for enter key is 13
+//Which method - For key or mouse events, this property indicates the specific key or button that was pressed.
+$("input[type = 'text']").keypress(function(event){
+  if(event.which === 13){
+    //grab new todo text from input
+    var todoText = $(this).val();
+    $(this).val("");
+    //Add new todo to end of li
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    //Clear out the input field
+  }
 });
